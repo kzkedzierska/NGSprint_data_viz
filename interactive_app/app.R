2914c60747f997ae4c79fa98e5eb7823a882ebc7
kzkedzierska <kzkedzierska@gmail.com>
2021-09-15 00:47:26 +0100

    Improved documentation in shiny app prep; removed per patient plot and improved shiny app by adding plot per subtype and changing layout

--- interactive_app/app.R
+++ interactive_app/app.R
@@ -9,107 +9,110 @@
 
 library(shiny)
 library(tidyverse)
+library(ggsci)
 source("cosmic_genes.R") # I copied cosmic genes there, 
 # read more about COSMIC census here: https://cancer.sanger.ac.uk/census
 # essentially this is a list of genes recognized as cancer genes
 
+# Things called only once
+# Read in data
 maf_data <- readRDS("data/maf_data_df.RDS")
 normalized_counts <- readRDS("data/normalized_counts.RDS")
-patient_ids <- colnames(normalized_counts)
+tcga_subtype_data <- readRDS("data/tcga_subtype_data.RDS")
 
-# Define UI for application that draws a histogram
+# Define constants
+patient_ids <- 
+    colnames(normalized_counts)
+
+patients_with_mut_data <-
+    maf_data %>%
+    mutate(patient_id = str_extract(Tumor_Sample_Barcode,
+                                    "[^-]{4}-[^-]{2}-[^-]{4}")) %>%
+    pull(patient_id) %>%
+    unique()
+
+# Interactive part
+# Define user interface (UI) for application
 ui <- fluidPage(
-
+    
     # Application title
     titlePanel("LGG - interactive expression plots"),
-
-    # Sidebar with a slider input for number of bins 
+    
     sidebarLayout(
         sidebarPanel(
             selectizeInput(inputId = "genes_to_plot",
-                        label = "Genes to plot:",
-                        choices = cosmic_genes,
-                        selected = "IDH1", 
-                        options = list(maxItems = 8)),
-            selectizeInput(inputId = "patients_to_plot",
-                           label = "Pateints to plot:",
-                           choices = patient_ids,
-                           selected = c("TCGA-HW-7495", "TCGA-DU-6405"), 
-                           options = list(maxItems = 8))
+                           label = "Genes to plot:",
+                           choices = cosmic_genes,
+                           selected = c("IDH1", "TP53"), 
+                           multiple = TRUE,
+                           options = list(maxItems = 10))
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("expr_mut_plot"),
-           plotOutput("expr_mut_patient_plot")
+            tabsetPanel(
+                tabPanel(
+                    "Expression by mutation status",
+                    verbatimTextOutput("mut", placeholder = FALSE),
+                    plotOutput("expr_mut_plot")
+                ),
+                tabPanel(
+                    "Expression by subtype",
+                    verbatimTextOutput("subtype", placeholder = FALSE),
+                    plotOutput("expr_subtype")
+                )
+            )
         )
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server that reacts to user input
 server <- function(input, output) {
     
-    output$expr_mut_patient_plot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        # which genes are we intersted in?
-        
-        patients_of_interest <- input$patients_to_plot
-        genes_of_interest <- input$genes_to_plot
-
-
-        expression_vals <-
-            normalized_counts[genes_of_interest, patients_of_interest, drop = FALSE] %>%
-            as.data.frame() %>%
-            rownames_to_column("Hugo_Symbol") %>%
-            pivot_longer(names_to = "patient_id",
-                         values_to = "norm_expression",
-                         cols = -Hugo_Symbol)
-        
-        
-        expression_vals %>%
-            ggplot(aes(Hugo_Symbol, norm_expression, fill = patient_id)) +
-            geom_bar(position = "dodge", stat = "identity") +
-            theme_classic()
-    })
-
+    # data frames recalculated with changing input
+    # expression
+    expression_vals <-
+        reactive({
+            normalized_counts[input$genes_to_plot, , drop = FALSE] %>%
+                as.data.frame() %>%
+                rownames_to_column("Hugo_Symbol") %>%
+                pivot_longer(names_to = "patient_id",
+                             values_to = "norm_expression",
+                             cols = -Hugo_Symbol)
+        })
+    # somatic mutations
+    mutation_data <-
+        reactive({
+            maf_data %>%
+                filter(Hugo_Symbol %in% input$genes_to_plot) %>%
+                mutate(patient_id = str_extract(Tumor_Sample_Barcode,
+                                                "[^-]{4}-[^-]{2}-[^-]{4}")) %>%
+                dplyr::select(patient_id, Hugo_Symbol, VARIANT_CLASS)
+        })
+    output$subtype <- 
+        renderText("Compare expression levels per subtype.")
+    
+    output$mut <- 
+        renderText("Compare expression levels per mutational status.")
+    
     output$expr_mut_plot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        # which genes are we intersted in?
         
-        genes_of_interest <- input$genes_to_plot
         # this will allow us to distinguish between no infomration about mutation
         # and no mutation
-        patient_muts <-
-            maf_data %>%
-            mutate(patient_id = str_extract(Tumor_Sample_Barcode,
-                                            "[^-]{4}-[^-]{2}-[^-]{4}")) %>%
-            pull(patient_id) %>%
-            unique()
-
-        expression_vals <-
-            normalized_counts[genes_of_interest,, drop = FALSE] %>%
-            as.data.frame() %>%
-            rownames_to_column("Hugo_Symbol") %>%
-            pivot_longer(names_to = "patient_id",
-                         values_to = "norm_expression",
-                         cols = -Hugo_Symbol)
-
-        mutation_data <-
-            maf_data %>%
-            filter(Hugo_Symbol %in% genes_of_interest) %>%
-            mutate(patient_id = str_extract(Tumor_Sample_Barcode,
-                                            "[^-]{4}-[^-]{2}-[^-]{4}")) %>%
-            dplyr::select(patient_id, Hugo_Symbol, VARIANT_CLASS)
 
         expr_mut_df <-
-            full_join(expression_vals, mutation_data) %>%
+            full_join(expression_vals(), mutation_data()) %>%
             # introduce WT for samples with no mutation in the gene
             mutate(VARIANT_CLASS = as.character(VARIANT_CLASS),
+                   # VARIANT_CLASS:
+                   # * not NA => VARIANT_CLASS, i.e. classification of mutation
+                   # * NA and id present in patients_with_mut_data => no mutation in gene
+                   # * NA and not in patients_with_mut_data => no mutation data for patient
                    VARIANT_CLASS = ifelse(is.na(VARIANT_CLASS),
-                                          yes = ifelse(patient_id %in% patient_muts,
+                                          yes = ifelse(patient_id %in% patients_with_mut_data,
                                                        yes = "WT",
                                                        no = "NA"),
                                           no = VARIANT_CLASS),
+                   
+                   # setting factors ordereing
                    VARIANT_CLASS = factor(VARIANT_CLASS,
                                           levels = c("WT",
                                                      "SNV",
@@ -117,18 +120,40 @@ server <- function(input, output) {
                                                      "NA")))
 
         expr_mut_df %>%
-            ggplot(aes(VARIANT_CLASS, norm_expression, color = VARIANT_CLASS,
+            ggplot(aes(VARIANT_CLASS, norm_expression, 
+                       color = VARIANT_CLASS,
                        shape = VARIANT_CLASS)) +
             geom_jitter() +
             geom_boxplot(color = "black", width = 0.3,
                          alpha = 0.7, outlier.shape = NA) +
             facet_wrap(~Hugo_Symbol) +
             theme_classic() +
-            theme(legend.position = "none") +
+            scale_color_simpsons() +
+            theme(legend.position = "none", 
+                  text = element_text(size = 20)) +
             labs(y = "vst normalized expression",
                  x = "Variant classification")
         
     })
+    
+    output$expr_subtype <-
+        renderPlot({
+            expression_vals() %>%
+                left_join(tcga_subtype_data, 
+                          by = c("patient_id" = "patient")) %>%
+                ggplot(aes(`Transcriptome.Subtype`, norm_expression, 
+                           color = `Transcriptome.Subtype`)) +
+                geom_jitter() +
+                geom_boxplot(color = "black", width = 0.3,
+                             alpha = 0.7, outlier.shape = NA) +
+                facet_wrap(~Hugo_Symbol) +
+                theme_classic() +
+                scale_color_futurama() +
+                theme(legend.position = "none", 
+                      text = element_text(size = 20)) +
+                labs(y = "vst normalized expression",
+                     x = "Transcriptome based subtype")
+        })
 }
 
 # Run the application 
